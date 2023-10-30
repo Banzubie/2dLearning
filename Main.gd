597102cd893e7303dbad91c789d97f55c1029bec
Brian Vose <BRVose@gmail.com>
2023-10-29 19:53:33 -0600

    basic version of game running

--- Main.gd
+++ Main.gd
@@ -14,14 +14,19 @@ func _process(delta):
 	pass
 
 
+
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message('Get Ready')
+	get_tree().call_group('mobs', 'queue_free')
 	
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
@@ -41,5 +46,18 @@ func _on_mob_timer_timeout():
 	
 	add_child(mob)
 	
+	$HUD.update_score(score)
 	
 	
+	
+
+
+func _on_score_timer_timeout():
+	score += 1
+	
+	
+
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
